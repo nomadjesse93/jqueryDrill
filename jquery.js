62b5d4693efd779875bc43462f5be14173226c52
unknown <jessev93@live.ca>
2018-08-06 21:13:15 +0700

    Stuck again :'(

--- jquery.js
+++ jquery.js
@@ -1,22 +1,48 @@
-$(document).ready(function () {
 
+$(document).ready(function () {
 
     $("#btnsubmit").on("click", function () {
         alert("Clicked Submit Button")
     });
 
-    $("#btnsubmit2").on("click", function(){
-       
-    alert( $("#text").val())
+    let div = $(document.createElement("div")).appendTo(document.body)
+    let h2 = $(document.createElement("h2"))
+    $(h2).appendTo(div)
+   
+function printtext(){
+    
+    let text = $(document.createTextNode($("#text").val()))
+    $(text).appendTo(h2)
+
+}
+
+
+    $("#btnsubmit2").on("click", function () {
+        printtext();
+        alert($("#text").val())
+        
+       });
+
+   
+     
+    
+      
+    
+    
+
+    
+    
+
+    if ($("#text").val() == 0) {
+        $("#btnsubmit2").prop("disabled", true)
+    }
+    
+    if ($("#text").keyup(function () {
+        $("#btnsubmit2").prop("disabled", false)
+    }));
+
+
+
 
 });
 
-if ($("#text").val() == 0){
-    $("#btnsubmit2").prop("disabled", true)
-}   
-if ($("#text").keyup(function(){
-    $("#btnsubmit2").prop("disabled",false)
-}));
-  
-
-});
