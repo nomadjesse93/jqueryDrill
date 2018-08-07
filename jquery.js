341eac465573e8d5ae4f8b068e55505d13a456da
unknown <jessev93@live.ca>
2018-08-07 20:16:49 +0700

    unstuck commit

--- jquery.js
+++ jquery.js
@@ -5,44 +5,41 @@ $(document).ready(function () {
         alert("Clicked Submit Button")
     });
 
-    let div = $(document.createElement("div")).appendTo(document.body)
-    let h2 = $(document.createElement("h2"))
-    $(h2).appendTo(div)
-   
-function printtext(){
-    
-    let text = $(document.createTextNode($("#text").val()))
-    $(text).appendTo(h2)
-
-}
 
 
     $("#btnsubmit2").on("click", function () {
+        event.preventDefault()
         printtext();
         alert($("#text").val())
-        
-       });
+    });
 
-   
-     
-    
-      
-    
-    
 
-    
-    
+
 
     if ($("#text").val() == 0) {
+
         $("#btnsubmit2").prop("disabled", true)
+        
     }
-    
     if ($("#text").keyup(function () {
+
         $("#btnsubmit2").prop("disabled", false)
-    }));
+    }))
 
 
 
+
+
+
+        function printtext() {
+
+            let div = $(document.createElement("div")).appendTo(document.body);
+            let h2 = $(document.createElement("h2"));
+            $(h2).appendTo(div);
+            let text = $(document.createTextNode($("#text").val()))
+            $(text).appendTo(h2)
+
+        };
 
 });
 
