fc92ea04dbf3285bc683fe19fd6f1c853b05e948
unknown <jessev93@live.ca>
2018-08-08 20:09:41 +0700

    Step 18 working

--- jquery.js
+++ jquery.js
@@ -1,57 +1,54 @@
-
 $(document).ready(function () {
 
     $("#btnsubmit").on("click", function () {
         alert("Clicked Submit Button")
     });
 
+    function makelist(){
+        let div = ($(document.createElement("div")));
+        
+        let ul = ($(document.createElement("ul")));
+    
+       let li =  ($(document.createElement("li")));
+
+       let txt = ($(document.createTextNode($("#text").val())))
+
+       $(txt).appendTo(li)
+       $(li).appendTo(ul)
+        $(ul).appendTo(div)
+        $(div).appendTo("body");
+    }
 
 
     $("#btnsubmit2").on("click", function () {
         event.preventDefault()
-        printtext();
+        makelist()
         alert($("#text").val())
+   
+        
+
     });
-
-
 
 
     if ($("#text").val() == 0) {
 
         $("#btnsubmit2").prop("disabled", true)
-        
+
     }
     if ($("#text").keyup(function () {
 
         $("#btnsubmit2").prop("disabled", false)
-    }))
+    }));
 
 
 
-        function printtext() {
+    
+    
 
-            let div = $(document.createElement("div")).addClass("div1").appendTo(document.body);
-            let h2 = $(document.createElement("h2")).addClass("h2");
-            $(h2).appendTo(div);
-            let text = $(document.createTextNode($("#text").val()))
-            $(text).appendTo(h2)
 
-            $(div).on("mouseover", function(){
-                $(div).css(
-                    { "background-color": "red",
-                    "border-radius":"1em",
-                "height":"15em",
-                "width":"15em",
-            "color": "blue"
-        });
-                $(div).on("mouseout", function(){
-$(div).css(
-    "color", "black"
-)
-                })
-            });
 
-        };
+
+
+
 
 });
-
