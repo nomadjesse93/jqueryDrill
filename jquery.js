61b2698a5870abf7ecc5b1acca907109b72de012
unknown <jessev93@live.ca>
2018-08-13 12:41:56 +0700

    Final Working Commit

--- jquery.js
+++ jquery.js
@@ -4,20 +4,49 @@ $(document).ready(function () {
         alert("Clicked Submit Button")
     });
 
+    let div = ($(document.createElement("div")));
+    let ul = ($(document.createElement("ul")));
+   
     function makelist(){
-        let div = ($(document.createElement("div")));
         
-        let ul = ($(document.createElement("ul")));
-    
-       let li =  ($(document.createElement("li")));
-
-       let txt = ($(document.createTextNode($("#text").val())))
-
-       $(txt).appendTo(li)
-       $(li).appendTo(ul)
+        
+        let li =  ($(document.createElement("li")));
+    $(li).addClass("words")
+        
+    let txt = ($(document.createTextNode($("#text").val())))
+       
+     
+        $(txt).appendTo(li)
+        $(li).appendTo(ul)
         $(ul).appendTo(div)
         $(div).appendTo("body");
+
+        $(li).on("click", function () {
+        
+            $(li).css({"color": getRandomColor()})
+            
+        });
+    
+        $(li).on("dblclick", function(){
+    $(li).remove()
+        })
+    
+       
     }
+
+    
+    
+
+    let color = $(["red","yellow","blue", "green", "orange"])
+    
+   
+   
+  function getRandomColor(){
+    let randomColor = color[Math.floor(Math.random() * color.length)]  
+    return randomColor;
+  }
+ 
+
 
 
     $("#btnsubmit2").on("click", function () {
